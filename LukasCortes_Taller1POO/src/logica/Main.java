c331c3e78aa7fe66fad757fb570eb74aaee1c1fe
Lukas Cortes <mrvro@NotebookToshi>
2026-03-29 19:26:51 -0300

    Metodo de Inicio de sesion y modificacion de metodo leerUsuarios

--- LukasCortes_Taller1POO/src/logica/Main.java
+++ LukasCortes_Taller1POO/src/logica/Main.java
@@ -5,20 +5,37 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 public class Main {
 	public static Scanner sc;
+	//Se inician variables para usar luego
+	static String[] usuarios = new String[10];
+	static String[] contraseñas = new String[10];
+	static int cantUsuarios = 0;
+	static String usuarioActivo = "";
 	public static void main(String[] args) throws FileNotFoundException {
 		leerRegistros(); //Lectura del archivo Registros.txt
 		leerUsuarios();	//Lectura del archivo Usuarios.txt
-		
 		int opcion = 0; 
-		sc = new Scanner(System.in);
+		
+		sc = new Scanner(System.in); //Se crea un nuevo escaner para poder ingresar por consola
 		do {
 			System.out.println("1) Menu de Usuarios");
 			System.out.println("2) Menu de Analisis");
 			System.out.println("3) Salir");
 			opcion = sc.nextInt();
+			sc.nextLine(); //Para saltar la linea y que no hayan errores luego al seguir usando sc
 			switch (opcion) {
 			case 1:
-				menuUsuarios();
+				//Ingreso de usuario y contraseña
+				System.out.print("Usuario: ");
+				String usuario = sc.nextLine();
+				System.out.print("Contraseña: ");
+				String contraseña = sc.nextLine();
+				if (inicioSesion(usuario, contraseña) == true) {//Llamamos al metodo que comprueba si existe el perfil ingresado
+					System.out.println("Acceso correcto!");
+					
+				}else {
+					System.out.println("Acceso incorrecto!");
+				}
+			
 				break;
 			case 2:
 				menuAnalisis();
@@ -32,27 +49,38 @@ public class Main {
 			}
 		}while (opcion != 3 || opcion == 2 || opcion == 1);
 	}
-	private static void menuUsuarios() {
+	private static boolean inicioSesion(String usuario, String contraseña) {
+		for (int i = 0; i < cantUsuarios; i++) {
+			if (usuario.equals(usuarios[i])) {
+				if(contraseña.equals(contraseñas[i])) {
+					usuarioActivo = usuarios[i]; //Guardamos el usuario de quien inicio sesion
+					return true; //Si se encontró usuario y contraseña y estan conectados retorna true
+				}else {
+					return false; //Si no encuentra la contraseña conectada al usuario ingresado retorna false
+				}	
+			}
+		}return false; //Si no encuentra el usuario en nuestra lista de usuarios retorna false
+	}
+	public static void menuAnalisis() {
 		// TODO Auto-generated method stub
 		
 	}
-	private static void menuAnalisis() {
-		// TODO Auto-generated method stub
-		
-	}
-	private static void leerUsuarios() throws FileNotFoundException {
-		File arch = new File("Usuarios.txt");
+	public static void leerUsuarios() throws FileNotFoundException {
+		File arch = new File("datos/Usuarios.txt");
 		sc = new Scanner(arch);
 		while (sc.hasNextLine()) {
 			String linea = sc.nextLine();
 			String[] partes = linea.split(";");
 			String ID = partes[0].trim();
 			String contraseña = partes[1].trim();
+			usuarios[cantUsuarios] = partes[0];
+			contraseñas[cantUsuarios] = partes[1];
+			cantUsuarios++;
 		}
 		
 	}
 	public static void leerRegistros() throws FileNotFoundException{
-		File arch = new File("Registros.txt");
+		File arch = new File("datos/Registros.txt");
 		sc = new Scanner(arch);
 		while (sc.hasNextLine()) {
 			String linea = sc.nextLine();
